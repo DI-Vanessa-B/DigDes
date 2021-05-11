e54e8e296b20516aa18e65423415a6338922b5c1
DI-Vanessa-B <75372128+DI-Vanessa-B@users.noreply.github.com>
2021-05-11 17:10:31 +0200

    tidy up of code

--- Max_Frisch_Anim.js
+++ Max_Frisch_Anim.js
@@ -1,16 +1,16 @@
-(function(h, f) {
+(function (h, f) {
     "function" === typeof define && define.amd ? define([], f) : "object" === typeof module && module.exports ? module.exports = f() : h.Rellax = f()
-})(this, function() {
-    var h = function(f, l) {
+})(this, function () {
+    var h = function (f, l) {
         var b = Object.create(h.prototype),
             g = 0,
             k = 0,
             c = [],
             p = !1,
-            u = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame || function(a) {
+            u = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame || function (a) {
                 setTimeout(a, 1E3 / 60)
             },
-            m = function(a, b, d) {
+            m = function (a, b, d) {
                 return a <= b ? b : a >= d ? d : a
             };
         b.options = {
@@ -18,7 +18,7 @@
             center: !1
         };
         l &&
-            Object.keys(l).forEach(function(a) {
+            Object.keys(l).forEach(function (a) {
                 b.options[a] = l[a]
             });
         b.options.speed = m(b.options.speed, -10, 10);
@@ -26,7 +26,7 @@
         var q = document.querySelectorAll(f);
         if (0 < q.length) b.elems = q;
         else throw Error("The elements you're trying to select don't exist.");
-        var v = function(a) {
+        var v = function (a) {
                 var e = a.getAttribute("data-rellax-percentage"),
                     d = a.getAttribute("data-rellax-speed"),
                     c = e || b.options.center ? window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop : 0,
@@ -50,34 +50,34 @@
                     transform: d
                 }
             },
-            r = function() {
+            r = function () {
                 var a = g;
                 g = void 0 !== window.pageYOffset ? window.pageYOffset : (document.documentElement ||
                     document.body.parentNode || document.body).scrollTop;
                 return a != g ? !0 : !1
             },
-            t = function() {
+            t = function () {
                 r() && !1 === p && n();
                 u(t)
             },
-            n = function() {
+            n = function () {
                 for (var a = 0; a < b.elems.length; a++) {
                     var e = " translate3d(0," + (Math.round(100 * c[a].speed * (1 - (g - c[a].top + k) / (c[a].height + k))) - c[a].base) + "px,0)" + c[a].transform;
                     b.elems[a].style.cssText = c[a].style + "-webkit-transform:" + e + ";-moz-transform:" + e + ";transform:" + e + ";"
                 }
             };
-        b.destroy = function() {
+        b.destroy = function () {
             for (var a = 0; a < b.elems.length; a++) b.elems[a].style.cssText = c[a].style;
             p = !0
         };
-        (function() {
+        (function () {
             k = window.innerHeight;
             r();
             for (var a = 0; a < b.elems.length; a++) {
                 var e = v(b.elems[a]);
                 c.push(e)
             }
-            window.addEventListener("resize", function() {
+            window.addEventListener("resize", function () {
                 n()
             });
             t();
